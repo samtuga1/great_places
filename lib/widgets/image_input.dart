0370fdcda7d44cd80c95dd9f36886acf98e2617c
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-21 20:11:53 +0000

    update

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspath;
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({Key? key}) : super(key: key);
-
+  const ImageInput({Key? key, this.onSelectImage}) : super(key: key);
+  final Function? onSelectImage;
   @override
   State<ImageInput> createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+  Future<void> _takePicture() async {
+    final imageFile = await ImagePicker()
+        .pickImage(source: ImageSource.camera, maxHeight: 600);
+    setState(() {
+      _storedImage = File(imageFile!.path);
+    });
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile!.path);
+    final savedImage = await imageFile.saveTo('${appDir.path}/$fileName');
+    widget.onSelectImage!(savedImage);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -21,8 +36,11 @@ class _ImageInputState extends State<ImageInput> {
                   fit: BoxFit.cover,
                   width: double.infinity,
                 )
-              : const Text('No Image Taken'),
-          width: 100,
+              : const Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
+          width: 150,
           height: 100,
           decoration: BoxDecoration(
             border: Border.all(
@@ -38,7 +56,7 @@ class _ImageInputState extends State<ImageInput> {
         Expanded(
           child: FlatButton.icon(
             icon: const Icon(Icons.camera),
-            onPressed: () {},
+            onPressed: _takePicture,
             textColor: Theme.of(context).primaryColor,
             label: const Text('Take Picture'),
           ),
