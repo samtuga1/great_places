6d1c2f1bff987dec60cc8e5902def83c21e20dc8
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-21 18:23:21 +0000

    added image input widget

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({Key? key}) : super(key: key);
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : const Text('No Image Taken'),
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          alignment: Alignment.center,
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            icon: const Icon(Icons.camera),
+            onPressed: () {},
+            textColor: Theme.of(context).primaryColor,
+            label: const Text('Take Picture'),
+          ),
+        )
+      ],
+    );
+  }
+}
