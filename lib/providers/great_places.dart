8cbe32bf7ec4e8c087f733e87271bcd2c0dff3b9
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-21 20:11:25 +0000

    updates

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/foundation.dart';
 import '../models/place.dart';
+import 'dart:io';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _places = [];
   List<Place> get places {
     return [..._places];
+  }
+
+  void addPlace(String? pickedTile, File? pickedImage) {
+    final newPlace = Place(
+        id: DateTime.now().toString(),
+        title: pickedTile,
+        image: pickedImage,
+        location: null);
+    _places.add(newPlace);
+    notifyListeners();
   }
 }
