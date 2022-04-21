0ad255570ba945c8adf48397a01826b245c6522a
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-21 20:11:43 +0000

    update

--- lib/screens/place_list_screen.dart
+++ lib/screens/place_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:place_app/providers/great_places.dart';
+import 'package:provider/provider.dart';
 import '../screens/add_place_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
@@ -18,9 +20,24 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
-      ),
+      body: Consumer<GreatPlaces>(builder: (ctx, greatPlaces, ch) {
+        if (greatPlaces.places.isEmpty) {
+          return const Text('Got no places Yet... Add places');
+        } else {
+          return ListView.builder(
+            itemCount: greatPlaces.places.length,
+            itemBuilder: (ctx, i) => ListTile(
+              leading: CircleAvatar(
+                backgroundImage: FileImage(greatPlaces.places[i].image!),
+              ),
+              title: Text(greatPlaces.places[i].title!),
+              onTap: () {
+                // go to detail screen
+              },
+            ),
+          );
+        }
+      }),
     );
   }
 }
