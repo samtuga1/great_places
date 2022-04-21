de3746756883c312e1a8f05a48a331dbeb025929
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-21 18:23:10 +0000

    implemented add_place screen

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import '../widgets/image_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -9,18 +12,44 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Add Place')),
-      body: Column(children: [
-        const Text('Text fields...'),
-        ElevatedButton.icon(
-          onPressed: () {},
-          icon: const Icon(Icons.add),
-          label: const Text('Add Place'),
-        )
-      ]),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: const InputDecoration(
+                        label: Text('Title'),
+                      ),
+                      controller: _titleController,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const ImageInput()
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+            label: const Text('Add Place'),
+            color: Theme.of(context).accentColor,
+          )
+        ],
+      ),
     );
   }
 }
