7d684808b11ca529164c913237f1d5c8473d78be
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-21 20:11:37 +0000

    update

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:place_app/providers/great_places.dart';
 import '../widgets/image_input.dart';
+import 'dart:io';
+import 'package:provider/provider.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -12,7 +15,21 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  late File? _pickedImage;
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
   final _titleController = TextEditingController();
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage);
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,17 +53,19 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     const SizedBox(
                       height: 10,
                     ),
-                    const ImageInput()
+                    ImageInput(onSelectImage: _selectImage)
                   ],
                 ),
               ),
             ),
           ),
           RaisedButton.icon(
-            onPressed: () {},
+            onPressed: _savePlace,
             icon: const Icon(Icons.add),
             label: const Text('Add Place'),
             color: Theme.of(context).accentColor,
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
           )
         ],
       ),
